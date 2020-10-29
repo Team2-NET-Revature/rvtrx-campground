6a8eceadd90f44362cc6eadd462d2aeddc17c574
Jon Sledge <jonsledge39@gmail.com>
2020-10-29 18:38:41 -0400

    resolves merge conflict

--- angular/src/app/modules/account/account/account.component.ts
+++ angular/src/app/modules/account/account/account.component.ts
@@ -53,7 +53,14 @@ export class AccountComponent {
       (e) => editingService.update(e),
       (err) => {
         console.log(err);
+<<<<<<< HEAD
         this.callToastrError(err, 'Service Error');
+=======
+        this.toastrService.error(`${err.message}`, 'Service Error', {
+          disableTimeOut: true,
+          positionClass: 'toast-top-center',
+        });
+>>>>>>> resolves merge conflict
       }
     );
     // Register function for Payload release from editing service
