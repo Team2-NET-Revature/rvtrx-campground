60486baf9f92c84bd63be866f29745a96db02fbd
Alex Walker <atwalk14@gmail.com>
2020-11-10 18:41:11 -0500

    testing changes

--- angular/src/app/modules/account/account-booking/account-booking.component.spec.ts
+++ angular/src/app/modules/account/account-booking/account-booking.component.spec.ts
@@ -3,15 +3,25 @@ import { AccountBookingComponent } from './account-booking.component';
 import { booking } from '../../../data/Mocks/booking.mock';
 import { HttpClient } from '@angular/common/http';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { lodging } from '../../../data/Mocks/lodging.mock';
+import { Observable, of } from 'rxjs';
+import { Lodging } from 'data/lodging.model';
+import { LodgingService } from 'services/lodging/lodging.service';
 describe('AccountBookingComponent', () => {
   let component: AccountBookingComponent;
   let fixture: ComponentFixture<AccountBookingComponent>;
 
   beforeEach(
     waitForAsync(() => {
+      const lodgingServiceStub = {
+        getById(id: string): Observable<Lodging> {
+          return of(lodging);
+        },
+      };
+
       TestBed.configureTestingModule({
         declarations: [AccountBookingComponent],
-        providers: [HttpClient],
+        providers: [HttpClient, { provide: LodgingService, useValue: lodgingServiceStub }],
         imports: [HttpClientTestingModule],
       }).compileComponents();
     })
@@ -37,9 +47,9 @@ describe('AccountBookingComponent', () => {
     expect(component.imageUri).toBeTruthy();
     expect(component.lodgingName).toBeTruthy();
 
-    component.getImageUriByLodgingId(component.booking.lodgingId);
+    component.getImageUriByLodgingId(lodging.id);
     expect(component.getImageUriByLodgingId).toHaveBeenCalled();
-    expect(component.imageUri).not.toEqual('');
-    expect(component.lodgingName).not.toEqual('');
+    expect(component.imageUri).toEqual('https://bulma.io/images/placeholders/128x128.png');
+    expect(component.lodgingName).toEqual('test');
   });
 });
